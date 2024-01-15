7c5a5a2bf66f509714f3583db933847f9b41ce2a
Kim Hyeon Woo, Lery <doralife12@naver.com>
2024-01-16 01:55:33 +0900

    feat: 메시지 빌더 구현 (#52)
    * feat: 메시지 빌더 구현
    
    * refactor: 임포트 순서 수정

---
+++ src/core/message/MessageBuilder.spec.ts
@@ -0,0 +1,55 @@
+import { Test } from '@nestjs/testing';
+import { advanceTo, clear } from 'jest-date-mock';
+
+import { MessageBuilder } from '@sight/core/message/MessageBuilder';
+
+describe('MessageBuilder', () => {
+  let messageBuilder: MessageBuilder;
+
+  beforeAll(async () => {
+    advanceTo(new Date());
+
+    const testModule = await Test.createTestingModule({
+      providers: [MessageBuilder],
+    }).compile();
+
+    messageBuilder = testModule.get(MessageBuilder);
+  });
+
+  afterAll(() => {
+    clear();
+  });
+
+  test('파라미터가 없는 메시지는 그대로 반환해야 한다', () => {
+    const GIVEN_MESSAGE = 'Hello, World!';
+    const expected = GIVEN_MESSAGE;
+
+    const actual = messageBuilder.build(GIVEN_MESSAGE, {});
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('파라미터 1개 있는 메시지는 파라미터를 포함하여 반환해야 한다', () => {
+    const GIVEN_MESSAGE = 'Hello, :username:!';
+    const expected = 'Hello, Lery!';
+
+    const actual = messageBuilder.build(GIVEN_MESSAGE, { username: 'Lery' });
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('파라미터 5개 있는 메시지는 파라미터를 포함하여 반환해야 한다', () => {
+    const GIVEN_MESSAGE = ':some1: :some2: :some3: :some4: :some5:';
+    const expected = `1 2 3 4 5`;
+
+    const actual = messageBuilder.build(GIVEN_MESSAGE, {
+      some1: '1',
+      some2: '2',
+      some3: '3',
+      some4: '4',
+      some5: '5',
+    });
+
+    expect(actual).toEqual(expected);
+  });
+});
